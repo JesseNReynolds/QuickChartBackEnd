74a46f923da3e431888fae5a51542e9054d196c4
JesseNReynolds <jessenreynolds@gmail.com>
2021-01-26 11:45:28 -0700

    Seeded, index and show for songs returns what I want

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,103 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Composer.create(name: "Blues Standards")
+
+Song.create(name: "12 Bar Blues in A", composer_id: 1, object_notation: 
+'{
+    "tonic": "A",
+    "mode": "Major",
+    "timeSignature": "4/4",
+    "measures": [
+        {
+            "firstHalf":{
+                "interval": "1",
+                "modifier": ""
+            },
+            "secondHalf":{
+                "interval": "1",
+                "modifier": ""
+            }
+        },
+        {
+            "firstHalf":{
+                "interval": "1",
+                "modifier": ""
+            },
+            "secondHalf":{
+                "interval": "1",
+                "modifier": ""
+            }
+        },
+        {
+            "firstHalf":{
+                "interval": "1",
+                "modifier": ""
+            },
+            "secondHalf":{
+                "interval": "1",
+                "modifier": ""
+            }
+        },
+        {
+            "firstHalf":{
+                "interval": "4",
+                "modifier": ""
+            },
+            "secondHalf":{
+                "interval": "4",
+                "modifier": ""
+            }
+        }
+        {
+            "firstHalf":{
+                "interval": "1",
+                "modifier": ""
+            },
+            "secondHalf":{
+                "interval": "1",
+                "modifier": ""
+            }
+        },
+        {
+            "firstHalf":{
+                "interval": "5",
+                "modifier": ""
+            },
+            "secondHalf":{
+                "interval": "5",
+                "modifier": ""
+            }
+        }, {
+            "firstHalf":{
+                "interval": "4",
+                "modifier": ""
+            },
+            "secondHalf":{
+                "interval": "4",
+                "modifier": ""
+            }
+        }
+        {
+            "firstHalf":{
+                "interval": "1",
+                "modifier": ""
+            },
+            "secondHalf":{
+                "interval": "1",
+                "modifier": ""
+            }
+        },
+        {
+            "firstHalf":{
+                "interval": "5",
+                "modifier": ""
+            },
+            "secondHalf":{
+                "interval": "5",
+                "modifier": ""
+            }
+        }
+    ]
+}')
