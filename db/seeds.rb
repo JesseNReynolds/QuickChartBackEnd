472f3e14a536d53b7044c1cdb93dee6ad20f8cb7
JesseNReynolds <jessenreynolds@gmail.com>
2021-01-26 15:53:14 -0700

    got it

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,97 +10,97 @@ Composer.create(name: "Blues Standards")
 
 Song.create(name: "12 Bar Blues in A", composer_id: 1, properties: 
 '{
-    tonic: "A",
-    mode: "Major",
-    timeSignature: "4/4",
-    measures: [
+    "tonic": "A",
+    "mode": "Major",
+    "timeSignature": "4/4",
+    "measures": [
         {
-            firstHalf: {
-                interval: "1",
-                modifier: ""
+            "firstHalf":{
+                "interval": "1",
+                "modifier": ""
             },
-            secondHalf: {
-                interval: "1",
-                modifier: ""
+            "secondHalf":{
+                "interval": "1",
+                "modifier": ""
             }
         },
         {
-            firstHalf: {
-                interval: "1",
-                modifier: ""
+            "firstHalf":{
+                "interval": "1",
+                "modifier": ""
             },
-            secondHalf: {
-                interval: "1",
-                modifier: ""
+            "secondHalf":{
+                "interval": "1",
+                "modifier": ""
             }
         },
         {
-            firstHalf: {
-                interval: "1",
-                modifier: ""
+            "firstHalf":{
+                "interval": "1",
+                "modifier": ""
             },
-            secondHalf: {
-                interval: "1",
-                modifier: ""
+            "secondHalf":{
+                "interval": "1",
+                "modifier": ""
             }
         },
         {
-            firstHalf: {
-                interval: "4",
-                modifier: ""
+            "firstHalf":{
+                "interval": "4",
+                "modifier": ""
             },
-            secondHalf: {
-                interval: "4",
-                modifier: ""
-            }
-        }
-        {
-            firstHalf: {
-                interval: "1",
-                modifier: ""
-            },
-            secondHalf: {
-                interval: "1",
-                modifier: ""
+            "secondHalf":{
+                "interval": "4",
+                "modifier": ""
             }
         },
         {
-            firstHalf: {
-                interval: "5",
-                modifier: ""
+            "firstHalf":{
+                "interval": "1",
+                "modifier": ""
             },
-            secondHalf: {
-                interval: "5",
-                modifier: ""
+            "secondHalf":{
+                "interval": "1",
+                "modifier": ""
+            }
+        },
+        {
+            "firstHalf":{
+                "interval": "5",
+                "modifier": ""
+            },
+            "secondHalf":{
+                "interval": "5",
+                "modifier": ""
             }
         }, {
-            firstHalf: {
-                interval: "4",
-                modifier: ""
+            "firstHalf":{
+                "interval": "4",
+                "modifier": ""
             },
-            secondHalf: {
-                interval: "4",
-                modifier: ""
-            }
-        }
-        {
-            firstHalf: {
-                interval: "1",
-                modifier: ""
-            },
-            secondHalf: {
-                interval: "1",
-                modifier: ""
+            "secondHalf":{
+                "interval": "4",
+                "modifier": ""
             }
         },
         {
-            firstHalf: {
-                interval: "5",
-                modifier: ""
+            "firstHalf":{
+                "interval": "1",
+                "modifier": ""
             },
-            secondHalf: {
-                interval: "5",
-                modifier: ""
+            "secondHalf":{
+                "interval": "1",
+                "modifier": ""
+            }
+        },
+        {
+            "firstHalf":{
+                "interval": "5",
+                "modifier": ""
+            },
+            "secondHalf":{
+                "interval": "5",
+                "modifier": ""
             }
         }
     ]
